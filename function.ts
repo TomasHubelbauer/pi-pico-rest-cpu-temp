cb9e14af0647c1446b0c361b469e8cdb656cd9c9
Tomáš Hübelbauer <tomas@hubelbauer.net>
2022-09-22 22:04:01 +0200

    Make the function connect to the database and print the data
    This is the basis for adding the data later once I parse them out of the request.

--- function.ts
+++ function.ts
@@ -1,7 +1,28 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
 
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
+// Pull the database connection string from the Deno environment variables
+const connectionString = Deno.env.get("CONNECTION_STRING")!;
+
+// Set up a connection pool with a single lazy connection
+// TODO: Convert this to use a `Client` if it can be made as brief as a pool
+const pool = new postgres.Pool(connectionString, 1, true);
+
+serve(async request => {
+  const connection = await pool.connect();
+  try {
+    const data = await connection.queryObject`SELECT * FROM temperature`;
+    console.log(data);
+    return new Response("Success: " + data, {
+      headers: { "content-type": "text/plain" },
+    });
+  }
+  catch (error) {
+    return new Response("Error: " + error.message, {
+      headers: { "content-type": "text/plain" },
+    });
+  }
+  finally {
+    connection.release();
+  }
 });
