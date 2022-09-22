ee61df8d64ed66241df9a81bb45a56c4750281eb
Tomáš Hübelbauer <tomas@hubelbauer.net>
2022-09-22 21:54:13 +0200

    Set up a hello world function
    This is to test deployments get kicked off following repository pushes.

--- function.ts
+++ function.ts
@@ -1,1 +1,7 @@
-console.log('todo');
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+serve((_req) => {
+  return new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+});
