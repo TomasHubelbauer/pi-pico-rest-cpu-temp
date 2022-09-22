d269258589bcebef30ce4189bbb1c1fbad32629b
Tomáš Hübelbauer <tomas@hubelbauer.net>
2022-09-22 22:55:18 +0200

    Move the client setup into the try-catch to get the proper error handling
    I think the function would have hung otherwise maybe.

--- function.ts
+++ function.ts
@@ -1,20 +1,16 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
 
-// Pull the database connection string from the Deno environment variables
 const CONNECTION_STRING = Deno.env.get("CONNECTION_STRING")!;
-
-// Pull the authorization secret from the Deno environment variables
 const SECRET = Deno.env.get("SECRET")!;
 
-// Set up a connection client
-const client = new postgres.Client(CONNECTION_STRING);
-
 serve(async request => {
-  await client.connect();
   try {
-    // Parse out the temperature value and the secret to authorize the caller
+    const client = new postgres.Client(CONNECTION_STRING);
+    await client.connect();
+    
     const { secret, temperature } = await request.json();
+    
     if (secret !== SECRET) {
       throw new Error('Invalid secret!');
     }
@@ -26,7 +22,7 @@ serve(async request => {
     const data = await client.queryObject`INSERT INTO temperature(temperature, recorded_at) VALUES (${temperature}, ${new Date()})`;
     console.log(data);
     
-    return new Response("Success: " + data.rowCount + " rows", {
+    return new Response("Success: " + data.rowCount, {
       headers: { "content-type": "text/plain" },
     });
   }
